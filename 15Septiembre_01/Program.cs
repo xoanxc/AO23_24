81392853087b69c2a56d277d5baa5b5dc3914e38
xoan.xc <xoanalonsofernandez@gmail.com>
2023-09-15 12:40:05 +0200

    Sep

---
+++ 15Septiembre_01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15Septiembre_01
+{
+        class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Programa que lee dos números y calcula la suma
+            //Declaración de variables:
+            int a, b, suma;
+            //  System.Int32 a1, b1, suma1; (No es importante, solo para fardar con los colegas)
+            // Innicializar a y b
+            a = 9;
+            b = 8;
+            //Puedo calcular la suma
+            suma = a + b;
+            //Escribir el resultado
+            Console.WriteLine("La suma es: " + suma);
+            Console.ReadLine();
+
+
+        }
+    }
+}
