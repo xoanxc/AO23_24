9be818c990736ad7ef64520cce08f6d8818781cc
xoan.xc <xoanalonsofernandez@gmail.com>
2023-09-20 13:05:06 +0200

    Sumary

---
+++ Tema 2/X/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace X
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("XXXX      XXXX    OOOOOOOOOOOO           AA");
+            Console.WriteLine(" XXXX    XXXX    OOOOO    OOOOO         AAAA");
+            Console.WriteLine("  XXXX  XXXX     OOOO      OOOO        AA  AA");
+            Console.WriteLine("   XXXXXXXX      OOO        OOO       AA    AA");
+            Console.WriteLine("   XXXXXXXX      OOO        OOO      AAAAAAAAAA       NNN");
+            Console.WriteLine("  XXXX  XXXX     OOOO      OOOO     AAAAAAAAAAAA      NNN");
+            Console.WriteLine(" XXXX    XXXX    OOOOO    OOOOO    AAA        AAA     NNN");
+            Console.WriteLine("XXXX      XXXX    OOOOOOOOOOOO    AAA          AAA    NNN");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Tema 3/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_TiposVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Declaración de variables
+            byte n1;
+            //GacInstalled byte n2
+        }
+    }
+}
