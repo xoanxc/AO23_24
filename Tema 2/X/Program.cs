ac5b57860d609319698766e055435541df91e09d
xoan.xc <xoanalonsofernandez@gmail.com>
2023-09-21 09:00:12 +0200

    Sum_21/09

--- Tema 2/X/Program.cs
+++ Tema 2/X/Program.cs
@@ -10,14 +10,27 @@ namespace X
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("XXXX      XXXX    OOOOOOOOOOOO           AA");
-            Console.WriteLine(" XXXX    XXXX    OOOOO    OOOOO         AAAA");
-            Console.WriteLine("  XXXX  XXXX     OOOO      OOOO        AA  AA");
-            Console.WriteLine("   XXXXXXXX      OOO        OOO       AA    AA");
-            Console.WriteLine("   XXXXXXXX      OOO        OOO      AAAAAAAAAA       NNN");
-            Console.WriteLine("  XXXX  XXXX     OOOO      OOOO     AAAAAAAAAAAA      NNN");
-            Console.WriteLine(" XXXX    XXXX    OOOOO    OOOOO    AAA        AAA     NNN");
-            Console.WriteLine("XXXX      XXXX    OOOOOOOOOOOO    AAA          AAA    NNN");
+            Console.WriteLine("XXXX      XXXX    OOOOOOOOOOOO           AA           NNNN     NNN");
+            Console.WriteLine(" XXXX    XXXX    OOOOO    OOOOO         AAAA          NNNNN    NNN");
+            Console.WriteLine("  XXXX  XXXX     OOOO      OOOO        AA  AA         NNN N    NNN");
+            Console.WriteLine("   XXXXXXXX      OOO        OOO       AA    AA        NNN  N   NNN");
+            Console.WriteLine("   XXXXXXXX      OOO        OOO      AAAAAAAAAA       NNN   N  NNN");
+            Console.WriteLine("  XXXX  XXXX     OOOO      OOOO     AAAAAAAAAAAA      NNN    N NNN");
+            Console.WriteLine(" XXXX    XXXX    OOOOO    OOOOO    AAA        AAA     NNN    NNNNN");
+            Console.WriteLine("XXXX      XXXX    OOOOOOOOOOOO    AAA          AAA    NNN     NNNN");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("          AA          LLLL            OOOOOOOOOOOO      NNNN     NNN       SSSSSSSS       OOOOOOOOOOOO");
+            Console.WriteLine("         AAAA         LLLL           OOOOO    OOOOO     NNNNN    NNN      SSSSSSSSSSS    OOOOO    OOOOO");
+            Console.WriteLine("        AA  AA        LLLL           OOOO      OOOO     NNN N    NNN       SSSS          OOOO      OOOO");
+            Console.WriteLine("       AA    AA       LLLL           OOO        OOO     NNN  N   NNN         SSSS        OOO        OOO");
+            Console.WriteLine("      AAAAAAAAAA      LLLL           OOO        OOO     NNN   N  NNN           SSSS      OOO        OOO");
+            Console.WriteLine("     AAAAAAAAAAAA     LLLLL          OOOO      OOOO     NNN    N NNN             SSSS    OOOO      OOOO");
+            Console.WriteLine("    AAA        AAA    LLLLLLLLLLL    OOOOO    OOOOO     NNN    NNNNN      SSSSSSSSSSSS   OOOOO    OOOOO");
+            Console.WriteLine("   AAA          AAA   LLLLLLLLLLL     OOOOOOOOOOOO      NNN     NNNN       SSSSSSSSSS     OOOOOOOOOOOO");
+
+
             Console.ReadLine();
         }
     }
