552f593fac1d758a7d6c0d882ab0dab562e19682
xoan.xc <xoanalonsofernandez@gmail.com>
2023-10-02 12:18:37 +0200

    Sum_1 (02/10/2023)

---
+++ Tema 3/Ejercicio 5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Programa que calcula el precio de los productos
+            double precio;
+            float cantidad;
+            string total;
+
+
+            Console.WriteLine("Introduzca el Precio: ");
+            precio = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Introduzca la Cantidad: ");
+            cantidad = float.Parse(Console.ReadLine());
+
+            total = (precio * cantidad).ToString();
+
+            Console.WriteLine(("El precio del producto es" + precio) ("euros y la cantidad que compra es" + cantidad) ("por lo tanto debe pagar" + total) ("euros.Vuelva pronto."));
+
+            Console.WriteLine();
+            Console.ReadLine();
+
+        }
+    }
+}
--- Tema 3/Ejercicio 9/Properties/AssemblyInfo.cs
+++ Tema 3/Ejercicio 9/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada a un ensamblado.
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("Ejercicio 9")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyProduct("Ejercicio 9")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("be5c556b-4dd5-437b-bb60-ac6dae5a10ae")]
+[assembly: Guid("2aa2eb87-ce34-4695-af27-1662652f2184")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
