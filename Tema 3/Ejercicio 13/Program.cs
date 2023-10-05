8264d275b8db688ce838acf62d9448a17a6bab92
xoan.xc <xoanalonsofernandez@gmail.com>
2023-10-05 18:40:45 +0200

    Sum_07 (05/10/2023)

--- Tema 3/Ejercicio 13/Program.cs
+++ Tema 3/Ejercicio 13/Program.cs
@@ -10,15 +10,31 @@ namespace Ejercicio_13
     {
         static void Main(string[] args)
         {
+
+            Console.Title = "Calculo de la Hipotenusa";
+
             //Toma de variable a
             Console.WriteLine("Valor del lado a: ");
             float a = float.Parse(Console.ReadLine());
+
+            //Espacio
+            Console.WriteLine();
 
             //Toma de variable b
             Console.WriteLine("Valor del lado b: ");
             float b = float.Parse(Console.ReadLine());
 
             //Calculo
+            float h = (float)Math.Pow((a + b), 2);
+
+            //Espacio
+            Console.WriteLine();
+
+            //Escribir el resultado
+            Console.WriteLine("La hipotenusa es: " + h);
+
+            //Evitar que se cierre
+            Console.ReadLine();
 
         }
     }
