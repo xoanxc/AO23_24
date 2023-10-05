1305e6178cbca3c5c57e4b6bf23d53ea02e6b4e1
xoan.xc <xoanalonsofernandez@gmail.com>
2023-10-05 13:42:49 +0200

    Sum_05 (05/10/2023)

---
+++ Tema 3/Ejercicio 15/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.Title = "Calcular";
+            //Recogida de Datos
+            Console.WriteLine("Lado del cubo: ");
+            float lado = float.Parse(Console.ReadLine());
+
+            //Espacio
+            Console.WriteLine();
+
+            //Calculo de datos
+            float D = (float)Math.Sqrt(3) * (lado);
+            float Al = (float)Math.Sqrt(lado) (2)) * (4);
+            floa
+
+
+            Console.WriteLine("La Diagonal es " + D + " la Al es " + Al + " y el V es " + V);
+
+        }
+    }
+}
