912873f13fc0a21305985c679d9f762c9ff0a83e
xoan.xc <xoanalonsofernandez@gmail.com>
2023-10-05 13:13:40 +0200

    Sum_01 (05/10/2023)

---
+++ Tema 3/ConsoleApp13/Program.cs
@@ -0,0 +1,58 @@
+﻿class Ejemplo
+{
+    public static void Main(string[] args)
+    {
+        /*
+        int n1, n2;
+        string lector = "0";
+        Console.WriteLine("Escribe el primer numero: ");
+        lector = Console.ReadLine();
+        n1 = Int32.Parse(lector);
+        Console.WriteLine("Escribe el segundo numero: ");
+        lector = Console.ReadLine();
+        n2 = Int32.Parse(lector);
+        Console.WriteLine("Que operación quiere realizar");
+        Console.WriteLine("1 - Suma");
+        Console.WriteLine("2 - resta");
+        Console.WriteLine("3 - multiplicación");
+        Console.WriteLine("4 - división");
+        Console.WriteLine("5 - Salir");
+
+        int opcion = int.Parse(Console.ReadLine());
+        string op = "El resultado de la operación es: ";
+        if (opcion == 1)
+        {
+            Console.WriteLine($"{op} {n1 + n2}");
+        }
+        else if (opcion == 2)
+        {
+            Console.WriteLine($"{op} {n1 - n2}");
+
+        }
+        else if (opcion == 3)
+        {
+            Console.WriteLine($"{op} {n1 * n2}");
+        }
+        else if (opcion == 4)
+        {
+            Console.WriteLine($"{op} {n1 / n2}");
+        }
+        else if (opcion == 5)
+        {
+            Console.WriteLine("Has cancelado la operación, ya puedes cerrar la ventana.");
+        }
+        else
+        {
+            Console.WriteLine("Valor inválido.");
+        }
+
+        */
+        int edad = 23;
+        double peso = 90.4;
+        Console.WriteLine("\t Tiene {0,2:d} años y pesa {1,6:f2} KG", edad, peso);
+
+
+
+
+    }
+}
---
+++ Tema 3/Ejercicio 14/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Precio del producto: ");
+            float precio = float.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Rebaja del producto: ");
+            float descuento = float.Parse(Console.ReadLine());
+
+            resultado = 
+
+
+        }
+    }
+}
