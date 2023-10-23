2628f081b71bbf5bc8663ce2915512d10ba63145
xoan.xc <xoanalonsofernandez@gmail.com>
2023-10-23 12:04:13 +0200

    Sum_01 (23/10/2023)

---
+++ Tema 4/Sentencia Switch/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sentencia_Switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char opcion;
+            char continuar;
+
+            do
+            {
+
+
+                Console.WriteLine("Escoge una opción:");
+                Console.WriteLine("a) Saludar");
+                Console.WriteLine("b) Animar");
+                Console.WriteLine("c) Despedir");
+                opcion = Char.Parse(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 'a':
+                        Console.WriteLine("Hola!!!");
+                        break;
+                    case 'b':
+                        Console.WriteLine("Vamos !!!");
+                        break;
+                    case 'c':
+                        Console.WriteLine("Hasta la próxima !!!");
+                        break;
+
+                    default:
+                        Console.WriteLine("Te has equivocado");
+                        break; //Opcional
+                }
+                Console.WriteLine("Quiere volver al menú: s/n");
+                continuar = Char.Parse(Console.ReadLine());
+                //continuar = Console.ReadKey().KeyChar; //Otra forma de hacerlo sin presionar enter
+
+            }
+
+            while (continuar == 's');
+            }
+    }
+}
