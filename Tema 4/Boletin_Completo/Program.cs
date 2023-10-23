6d6370d9fb3005e57628bf63273644b210a90177
xoan.xc <xoanalonsofernandez@gmail.com>
2023-10-23 13:03:22 +0200

    Sum_04 (23/10/2023)

--- Tema 4/Boletin_Completo/Program.cs
+++ Tema 4/Boletin_Completo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,7 +22,7 @@ namespace Boletin_Completo
 
                 int ejercicio = 0;
 
-                Console.WriteLine("Introducir ejercicios 4-8: ");
+                Console.WriteLine("Escoja un ejercicio de 4 al 23");
                 ejercicio = int.Parse(Console.ReadLine());
 
                 if (ejercicio == 4)
@@ -71,7 +72,6 @@ namespace Boletin_Completo
                         Console.WriteLine("No se puede calcular la raíz cuadrada de una suma negativa.");
                     }
                 }
-
                 else if (ejercicio == 6)
                 { //Ejercicio 6 - Xoán Alonso Fernández
 
@@ -153,16 +153,73 @@ namespace Boletin_Completo
                     else
                     {
                         Console.WriteLine("Todo en orden");
+                    }
+                    if (ejercicio == 9)
+                    {
+
+                        Console.WriteLine("Introduce un Numero: "); //Recogida de datos
+                        double numero = double.Parse(Console.ReadLine());
+
+                        if (numero % 2 == 0)
+                        {
+                            Console.WriteLine("Es múltiplo de 2");
+                        }
+
+                        if (numero % 3 == 0)
+                        {
+                            Console.WriteLine("Es múltiplo de 3");
+                        }
+                    }
+
+                    else if (ejercicio == 10)
+                    {
+                        Console.WriteLine("Introduce una cantidad de Dinero: "); //Recogida de datos
+                        double numero2 = double.Parse(Console.ReadLine());
+
+                        if (numero2 <= 100)
+                        {
+                            double calculo1 = (numero2 * 0.90); //Calculo del 10%
+                            Console.WriteLine("El descuento es del 10% y el precio queda en " + calculo1);
+                        }
+
+                        if (numero2 >= 100)
+                        {
+                            double calculo2 = (numero2 * 0.85); //Calculo del 15&%
+                            Console.WriteLine("El descuento es del 15% y el precio queda en " + calculo2);
+                        }
+                    }
+                    else if (ejercicio == 11)
+                    {
+                        Console.WriteLine("Introduce un numero de 0 a 10: "); //Recogida de Datos
+                        double numero3 = double.Parse(Console.ReadLine());
+
+                        if (numero3 <= 0.0 || numero3 >= 10.0)
+                        {
+                            Console.WriteLine("Error en nota (tan difícil es poner un numero?)");
+                        }
+                        else
+
+                        {
+                            Console.WriteLine("Te equivocaste, esperemos que no se repita");
+                        }
 
                     }
 
-                    Console.WriteLine("Quiere volver al menú: s/n");
-                    continuar = Char.Parse(Console.ReadLine());
+
+
                 }
+
+                Console.WriteLine();
+                Console.WriteLine("Quiere volver al menú: s/n");
+                continuar = Char.Parse(Console.ReadLine());
+
 
             }
 
             while (continuar == 's');
+
+
+
 
         }
     }
