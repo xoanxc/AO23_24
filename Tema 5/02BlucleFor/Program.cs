99c207c57f6076d1dd4b8559e39190d15c3607a9
xoan.xc <xoanalonsofernandez@gmail.com>
2023-11-08 12:26:51 +0100

    Sum_01 (08/11/2023)

---
+++ Tema 5/02BlucleFor/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02BlucleFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bucle For
+            //Mostrar números entre 1 y 20 por pantalla
+            for (int i = 1; i <= 20; i = i + 1)
+            {
+                Console.WriteLine(i + " ");
+            }
+
+
+            //Calcular la suma de todos los números entre n y m
+            int n = 15;
+            int m = 30;
+            int suma; //n + n +1 + n+2 + n+3+ n+4 . . . .
+
+            //Intento 1
+            for (int suma = m + 1)
+            {
+                Console.WriteLine();
+            }
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
