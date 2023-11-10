2987beb34141aa431bdb6b583db85097054c3489
xoan.xc <xoanalonsofernandez@gmail.com>
2023-11-10 12:30:11 +0100

    Sum_01 (10/11/2023)

---
+++ Tema 5/Boletin5.Bucles/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boletin5.Bucles
+{
+    internal class Program
+    {
+        static void escribirTitular(String titular)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*******Boletín de Bucles*******");
+            char opcion;
+            do
+            {
+                escribirTitular("Escoja un ejercicio entre 5 y 25");
+                int ejercicio = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                switch (ejercicio)
+                {
+                    case 5:
+                        escribirTitular("ejercicio 5: ");
+                        int i = 320;
+
+                        while (i >= 160)
+                        {
+                            Console.WriteLine(i);
+                            i = i - 20;
+                        }
+                        Console.WriteLine();
+                        break;
+                    case 6:
+                        escribirTitular("ejercicio 6");
+
+                        for (int j = 0; j <= 100; j++)
+                        {
+                            if (j % 5 == 0 && j % 7 == 0)
+                            {
+                                Console.WriteLine(j + ' ');
+                            }
+                        }
+                        break;
+                    case 7:
+                        
+                        break;
+                    default:
+                        Console.WriteLine("Error, ejercicio inexistente.");
+                        break;
+                }
+
+                Console.WriteLine("Desea volver al menú inicial: s/n");
+                opcion = char.ToLower(char.Parse(Console.ReadLine()));
+            } while (opcion == 's');
+        }
+    }
+}
