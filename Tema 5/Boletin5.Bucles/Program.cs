4d58021d8d7439fbba63e0cb0505876d83d17a57
xoanxc <xoanalonsofernandez@gmail.com>
2023-11-11 15:55:42 +0100

    Sum_01 (11/11/2023)

--- Tema 5/Boletin5.Bucles/Program.cs
+++ Tema 5/Boletin5.Bucles/Program.cs
@@ -56,16 +56,99 @@ namespace Boletin5.Bucles
                     case 9:
 
                         break;
+
                     case 10: //Para el Lunes
-                        Console.WriteLine("Introduce un numero no negativo: ");
+                        int m;
 
-                        break;
+                        do
+                        {
+                            Console.Write("Introduce un número no negativo: ");
+                            string input = Console.ReadLine();
+
+                            if (!int.TryParse(input, out m))
+                            {
+                                Console.WriteLine("Error, introduce un número válido.");
+                                continue;
+                            }
+
+                            if (m < 0)
+                            {
+                                Console.WriteLine("Error, el número debe ser no negativo. Inténtalo de nuevo.");
+                            }
+                            else
+                            {
+                                int suma = CalcularSuma(m);
+                                Console.WriteLine("La suma de los primeros" + m + "números es: " + suma);
+                            }
+
+                        } while (m < 0);
+                }
+
+                static int CalcularSuma(int m)
+                {
+                    int suma = 0;
+                    for (int i = 1; i <= m; i++)
+                    {
+                        suma += i;
+                    }
+                    return suma;
+                    break; //Fin
+
                     case 11: //Para el Lunes
+                        Console.Write("Introduce un número positivo (n): ");
+                        if (int.TryParse(Console.ReadLine(), out int n) && n > 0)
+                        {
+                            double suma = CalcularSuma(n);
+                            Console.WriteLine($"La suma de la serie es: {suma:F4}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Por favor, introduce un número positivo válido.");
+                        }
+                    }
 
+                    static double CalcularSuma(int n)
+                    {
+                        double suma = 0.0;
+
+                        for (int i = 1; i <= n; i++)
+                        {
+                            suma += 1.0 / Math.Pow(i * 10 + 3, 3);
+                        }
+
+                        return suma;
                         break;
                     case 12: //Para el Lunes
 
+                            Console.Write("Introduce un número no negativo: ");
+                            if (int.TryParse(Console.ReadLine(), out int n) && n >= 0)
+                            {
+                                int sumaDivisores = CalcularSumaDivisores(n);
+                                Console.WriteLine("La suma de los divisores de" + n + "es:" + sumaDivisores);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Error, Introduce un número no negativo válido.");
+                            }
+                        }
+
+                        static int CalcularSumaDivisores(int n)
+                        {
+                            int suma = 0;
+
+                            for (int i = 1; i <= n; i++)
+                            {
+                                if (n % i == 0)
+                                {
+                                    suma += i;
+                                }
+                            }
+
+                            return suma;
+                        }
+
                         break;
+
                     default:
                         Console.WriteLine("Error, ejercicio inexistente.");
                         break;
