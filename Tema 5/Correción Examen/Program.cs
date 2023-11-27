8affd3e6c7618ed9cc6058f704fbe4fc58b79d7a
xoan.xc <xoanalonsofernandez@gmail.com>
2023-11-27 12:05:37 +0100

    Sum_03 (27/11/2023)

--- Tema 5/Correción Examen/Program.cs
+++ Tema 5/Correción Examen/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -39,6 +40,69 @@ namespace Correción_Examen
 
                     //Ejercicio 2
                     case 2:
+                        int n, m;
+
+                        do
+                        {
+                            Console.WriteLine("Intro n: ");
+                            n = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Intro m: ");
+                            m = int.Parse(Console.ReadLine());
+
+                            int contador = 0;
+
+                            for (i = n; i <= m; i++)
+                            {
+                                if (i % 2 == 1 && i % 7 == 0)
+                                {
+                                    Console.WriteLine(i + " ");
+                                    contador++;
+                                }
+                                if (contador == 10)
+                                {
+                                    break;
+                                }
+                            }
+
+                        }
+                        while (n >= m);
+                        break;
+
+                    //Ejercicio 3
+                    case 3:
+                        int n3;
+                        double suma = 0;
+                        Console.WriteLine("Intro valor de n: ");
+                        n3 = int.Parse(Console.ReadLine());
+
+                        for (i = 10; i <= n3; i++)
+                        {
+                            suma = suma + 1 + i / (Math.Pow(i, 3)) + 1;
+                        }
+                        break;
+
+                    //Ejercicio 4
+                    case 4:
+                        int n4, m4;
+                        Console.WriteLine("Intro n4: ");
+                        n4 = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Intro m4: ");
+                        m4 = int.Parse(Console.ReadLine());
+
+                        bool primos = true;
+
+                        for (i = 2; i < m4; i++)
+                        {
+                            if (n4 % i == 0 && m4 % i == 0)
+                            {
+                                Console.WriteLine("No son primos entre sí");
+                                break;
+                            }
+                        }
+                        if (primos)
+                        {
+                            Console.WriteLine("Son primos entre sí");
+                        }
 
                         break;
                 }
