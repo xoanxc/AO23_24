07921466a7df0815c2c436f30c741f4ba9257079
xoan.xc <xoanalonsofernandez@gmail.com>
2023-12-04 13:00:34 +0100

    Sum_02 (04/12/2023)

---
+++ Tema 6/03AlgoritmosAvanzados/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03AlgoritmosAvanzados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Retos de programación");
+            int ejercicios = int.Parse(Console.ReadLine());
+
+            switch (ejercicios)
+            {
+                case 1:
+                    //1º Algoritmo que indica si dos arrays son iguales: es decir, tienen el mismo tamaño y los mismos valores.
+                    bool iguales = false;
+
+                    int[] Valor1 = new int[5];
+                    Valor1[2] = 5;
+
+                    int[] Valor2 = new int[5];
+                    Valor2[3] = 5;
+
+                    for (int i = 0; i < Valor1.Length; i++)
+                    {
+
+                    }
+
+
+
+                    break;
+                case 2:
+                    //2º Algoritmo que obtiene la posición del primer número superior a la media del array.
+                    
+                    int[] ints = new int[5];
+
+                    break;
+                case 3:
+                    //3º Algoritmo que obtiene el número mas pequeño de un array.
+                    Random generador = new Random();
+
+                    int[] c3 = new int[10];
+
+                    for (int i = 0; i < c3.Length; i++)
+                    {
+                        if (int[i] < )
+                    }
+                    break;
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
