feff8745bcafcebced89e991eb4f4fefd2afec10
xoan.xc <xoanalonsofernandez@gmail.com>
2024-01-22 10:46:43 +0100

    Sum_01 (22/01/2024)

---
+++ MenuDoWhile/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuDoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(" ");
+                char opcion;
+                do
+                {
+                    Console.WriteLine("Selecciona un ejercicio entre");
+                    byte ejercicio = byte.Parse(Console.ReadLine());
+                    Console.WriteLine();
+                    switch (ejercicio)
+
+
+                {
+                }
+
+
+
+                Console.WriteLine();
+                    Console.WriteLine("¿Desea volver al menú inicial? s/n");
+                    opcion = char.ToLower(char.Parse(Console.ReadLine()));
+
+                }
+                while (opcion == 's');
+            }
+        }
+    }
---
+++ Tema 6/BoletinMatrizes2D/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoletinMatrizes2D
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[,] a = new int[3, 3];
+        }
+    }
+}
