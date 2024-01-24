818bf23362d0f15930c0f9c639737dab844743b1
xoan.xc <xoanalonsofernandez@gmail.com>
2024-01-24 11:38:43 +0100

    Sum_01 (24/01/2024

---
+++ Tema 7/01_EjemploFunciones/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_EjemploFunciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char letra = 's';
+            char caracter = ' ';
+            char caracter2 = '4';
+
+            int letraInt = Convert.ToInt32(letra);
+            int caracterInt = Convert.ToInt32(caracter);
+            int caracter2Int = Convert.ToInt32(caracter2);
+
+            Console.WriteLine(letra + " : " + letraInt);
+            Console.WriteLine(caracter + " : " + caracterInt);
+            Console.WriteLine(caracter2 + " : " + caracterInt);
+
+            //Funciones de clase Char
+            bool mayusculas = char.IsUpper('W');
+            bool mayucula = char.IsLower('W');
+            bool digito = char.IsDigit('4');
+            bool letraA = char.IsLetter('4');
+            char minuscula2 = char.ToLower(letra); //No modifica la variable letra, genera otro char con el caracter minúscula 
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Tema 7/02_String/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String frase = "El Celta cae derrotado nuevamente";
+
+
+            //Un String es en realidad un array de caracteres
+            //Cada caracter tiene asignado un indice
+            //Pero los String no se pueden modificar
+            char primerCaracter = frase[0];
+            char ultimoCaracter = frase[frase.Length - 1];
+
+            //Como recorro un string
+            int contadorAes;
+            for (int i = 0; i < frase.Length; i++)
+            {
+                if (frase[i] == 'a')
+                {
+                    contadorAes++;
+                }
+;
+            }
+            //indexOf --> Que posición ocupa un determinado caracter
+            int posPrimerEspacio = frase.IndexOf(' ');
+            //Buscar la posicion de la primera a despues de un espacio
+            int posicion = frase.IndexOf('a', posPrimerEspacio);
+
+            char[] vocales = { 'a', 'e', 'i' };
+            int posPrimeraVocal = frase.IndexOfAny(vocales);
+
+            //Substring --> Obtiene una subcadena del string principal
+            String subCadena = frase.Substring(posPrimerEspacio + 1);
+            Console.WriteLine("SubCadena: " + subCadena);
+
+            string subcadena2 = frase.Substring(posPrimerEspacio + 1, 5);
+            Console.WriteLine("SubCadena: " + subCadena);
+
+            //Split --> Trocea un String en base a un separador 
+            String[] palabras  = frase.Substring(posPrimerEspacio   + 1, 9);
+            Console.WriteLine("SubCadena: " + subCadena2);
+        }
+    }
+}
