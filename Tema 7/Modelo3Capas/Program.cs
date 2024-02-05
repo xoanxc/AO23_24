bb96ce2378e740e1ed30db4a68f6331717897f6f
xoan.xc <xoanalonsofernandez@gmail.com>
2024-02-05 12:36:48 +0100

    Sum_03 (05/02/2024)

--- Tema 7/Modelo3Capas/Program.cs
+++ Tema 7/Modelo3Capas/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Modelo3Capas
 {
@@ -10,6 +11,49 @@ namespace Modelo3Capas
     {
         static void Main(string[] args)
         {
+            String ruta = Directory.GetCurrentDirectory();
+            String rutaFichero = ruta + @"\series.txt";
+
+            String[] registros = new String[100];
+
+            //Resuperar la info del fichero series.txt
+            StreamReader lector = new StreamReader(rutaFichero);
+
+            String registro = "";
+            int posicion = 0;
+
+            while(registros != null)
+            {
+                registro = lector.ReadLine();
+
+                //Guardar el registro en la priemra posición vacia de la matriz
+                registros[posicion] = registro;
+                posicion++;
+            }
+
+
+            lector.Close();
+
+            //Mostrar por pantalla la matriz de string
+            foreach(String reg in registros)
+            {
+                if (reg != null)
+                { 
+                    Console.WriteLine(reg);
+                }
+            }
+
+            StreamWriter escritor = new StreamWriter(rutaFichero, true);
+            Console.WriteLine("Introduzca su serie: ");
+            string serie = Console.ReadLine();
+            Console.WriteLine("En que plataforma se emite?");
+            string plataforma = Console.ReadLine();
+            escritor.WriteLine(serie + "*" + plataforma);
+
+            escritor.Close();
+
+            Console.ReadLine();
+
         }
     }
 }
