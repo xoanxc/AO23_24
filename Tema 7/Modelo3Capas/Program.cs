5ceeb6c45673595b627fa921f9d48b6738afbec7
xoan.xc <xoanalonsofernandez@gmail.com>
2024-02-05 11:58:36 +0100

    Sum_01 (05/02/2024)

---
+++ Tema 7/Modelo3Capas/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo3Capas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
