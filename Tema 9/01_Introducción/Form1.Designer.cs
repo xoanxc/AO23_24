1d79b5cc3b7e02651d52dba0a520067e2551c134
xoan.xc <xoanalonsofernandez@gmail.com>
2024-03-18 10:36:43 +0100

    Sum_01 (18/03/2024)

---
+++ Tema 9/01_Introducción/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace _01_Introducción
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnPulsame = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDNI = new System.Windows.Forms.TextBox();
+            this.guardarDNI = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btnPulsame
+            // 
+            this.btnPulsame.Location = new System.Drawing.Point(143, 169);
+            this.btnPulsame.Name = "btnPulsame";
+            this.btnPulsame.Size = new System.Drawing.Size(137, 67);
+            this.btnPulsame.TabIndex = 0;
+            this.btnPulsame.Text = "Pulsame";
+            this.btnPulsame.UseVisualStyleBackColor = true;
+            this.btnPulsame.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(139, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "DNI:";
+            // 
+            // txtDNI
+            // 
+            this.txtDNI.Location = new System.Drawing.Point(264, 104);
+            this.txtDNI.Name = "txtDNI";
+            this.txtDNI.Size = new System.Drawing.Size(159, 26);
+            this.txtDNI.TabIndex = 2;
+            // 
+            // guardarDNI
+            // 
+            this.guardarDNI.Location = new System.Drawing.Point(348, 181);
+            this.guardarDNI.Name = "guardarDNI";
+            this.guardarDNI.Size = new System.Drawing.Size(121, 55);
+            this.guardarDNI.TabIndex = 3;
+            this.guardarDNI.Text = "Guardar DNI";
+            this.guardarDNI.UseVisualStyleBackColor = true;
+            this.guardarDNI.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(205, 307);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(381, 42);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.guardarDNI);
+            this.Controls.Add(this.txtDNI);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnPulsame);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPulsame;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDNI;
+        private System.Windows.Forms.Button guardarDNI;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Tema 9/01_Introducción/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _01_Introducción
+{
+    public partial class Form1 : Form
+    {
+
+        //Zona de declaración de variables
+
+        List<string> listaDNI = new List<string>();
+
+
+
+
+
+        //************************************************************
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Has pulsado el botón");
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            //Recoger lo que el usuario introduce en la caja de texto
+            string dni = txtDNI.Text;
+
+
+            if(dni == "")
+            {
+                MessageBox.Show("No has introducido ningún DNI", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtDNI.Focus(); //Poner el foco en la caja de texto
+                return; //Salir corriendo de la función
+            }
+            
+                //Añadir el DNI a la lista
+                dni = dni.Trim(); //Quitar espacios en blanco
+                dni = dni.ToUpper(); //Pasar a mayúsculas
+                listaDNI.Add(dni);
+
+            
+            
+
+        }
+    }
+}
