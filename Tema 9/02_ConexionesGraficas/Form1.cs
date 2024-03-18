7af6b8e622c2e79856a9d8980b454bc9f6e19429
xoan.xc <xoanalonsofernandez@gmail.com>
2024-03-18 11:59:55 +0100

    Sum_04 (18/03/2024)

--- Tema 9/02_ConexionesGraficas/Form1.cs
+++ Tema 9/02_ConexionesGraficas/Form1.cs
@@ -16,5 +16,10 @@ namespace _02_ConexionesGraficas
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
