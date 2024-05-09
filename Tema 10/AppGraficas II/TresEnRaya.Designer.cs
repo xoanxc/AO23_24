c35fac67835f815344103021dc8b6125646c419b
xoan.xc <61795755+xoanxc@users.noreply.github.com>
2024-05-09 14:03:21 +0200

    Sum_05

--- Tema 10/AppGraficas II/TresEnRaya.Designer.cs
+++ Tema 10/AppGraficas II/TresEnRaya.Designer.cs
@@ -51,6 +51,7 @@
             this.button1.Size = new System.Drawing.Size(133, 123);
             this.button1.TabIndex = 0;
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -61,6 +62,7 @@
             this.button2.Size = new System.Drawing.Size(133, 123);
             this.button2.TabIndex = 1;
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -71,6 +73,7 @@
             this.button3.Size = new System.Drawing.Size(133, 123);
             this.button3.TabIndex = 2;
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -81,6 +84,7 @@
             this.button4.Size = new System.Drawing.Size(133, 123);
             this.button4.TabIndex = 3;
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
@@ -91,6 +95,7 @@
             this.button5.Size = new System.Drawing.Size(133, 123);
             this.button5.TabIndex = 4;
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
@@ -101,6 +106,7 @@
             this.button6.Size = new System.Drawing.Size(133, 123);
             this.button6.TabIndex = 5;
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
@@ -111,6 +117,7 @@
             this.button7.Size = new System.Drawing.Size(133, 123);
             this.button7.TabIndex = 6;
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
@@ -121,6 +128,7 @@
             this.button8.Size = new System.Drawing.Size(133, 123);
             this.button8.TabIndex = 7;
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button9
             // 
@@ -131,6 +139,7 @@
             this.button9.Size = new System.Drawing.Size(133, 123);
             this.button9.TabIndex = 8;
             this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // rd1Jugador
             // 
--- Tema 10/AppGraficas II/TresEnRaya.cs
+++ Tema 10/AppGraficas II/TresEnRaya.cs
@@ -16,5 +16,211 @@ namespace AppGraficas_II
         {
             InitializeComponent();
         }
+
+        //Funcion para reiniciar el juego
+        public void reiniciarJuego()
+        {
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+            button5.Text = "";
+            button6.Text = "";
+            button7.Text = "";
+            button8.Text = "";
+            button9.Text = "";
+        }
+
+        public void comprobarGanador ()
+        {
+            //Comprobar si hay un ganador
+
+            //Primera fila
+            if (button1.Text == "X" && button2.Text == "X" && button3.Text == "X")
+            {
+                MessageBox.Show("Ganador: X");
+                reiniciarJuego();
+            }
+            else if (button1.Text == "O" && button2.Text == "O" && button3.Text == "O")
+            {
+                MessageBox.Show("Ganador: O");
+                reiniciarJuego();
+            }
+
+            //Segunda fila
+            if (button4.Text == "X" && button5.Text == "X" && button6.Text == "X")
+            {
+                MessageBox.Show("Ganador: X");
+                reiniciarJuego();
+            }
+            else if (button4.Text == "O" && button5.Text == "O" && button6.Text == "O")
+            {
+                MessageBox.Show("Ganador: O");
+                reiniciarJuego();
+            }
+            
+            //Tercera fila
+            if (button7.Text == "X" && button8.Text == "X" && button9.Text == "X")
+            {
+                MessageBox.Show("Ganador: X");
+                reiniciarJuego();
+            }
+            else if (button7.Text == "O" && button8.Text == "O" && button9.Text == "O")
+            {
+                MessageBox.Show("Ganador: O");
+                reiniciarJuego();
+            }
+
+            //Primera columna
+            if (button1.Text == "X" && button4.Text == "X" && button7.Text == "X")
+            {
+                MessageBox.Show("Ganador: X");
+                reiniciarJuego();
+            }
+            else if (button1.Text == "O" && button4.Text == "O" && button7.Text == "O")
+            {
+                MessageBox.Show("Ganador: O");
+                reiniciarJuego();
+            }
+
+            //Segunda columna
+            if (button2.Text == "X" && button5.Text == "X" && button8.Text == "X")
+            {
+                MessageBox.Show("Ganador: X");
+                reiniciarJuego();
+            }
+            else if (button2.Text == "O" && button5.Text == "O" && button8.Text == "O")
+            {
+                MessageBox.Show("Ganador: O");
+                reiniciarJuego();
+            }
+
+            //Tercera columna
+            if (button3.Text == "X" && button6.Text == "X" && button9.Text == "X")
+            {
+                MessageBox.Show("Ganador: X");
+                reiniciarJuego();
+            }
+            else if (button3.Text == "O" && button6.Text == "O" && button9.Text == "O")
+            {
+                MessageBox.Show("Ganador: O");
+                reiniciarJuego();
+            }
+
+            //Diagonal 1
+            if (button1.Text == "X" && button5.Text == "X" && button9.Text == "X")
+            {
+                MessageBox.Show("Ganador: X");
+                reiniciarJuego();
+            }
+            else if (button1.Text == "O" && button5.Text == "O" && button9.Text == "O")
+            {
+                MessageBox.Show("Ganador: O");
+                reiniciarJuego();
+            }
+
+            //Diagonal 2
+            if (button3.Text == "X" && button5.Text == "X" && button7.Text == "X")
+            {
+                MessageBox.Show("Ganador: X");
+                reiniciarJuego();
+            }
+            else if (button3.Text == "O" && button5.Text == "O" && button7.Text == "O")
+            {
+                MessageBox.Show("Ganador: O");
+                reiniciarJuego();
+            }
+
+            //Empate
+            if (button1.Text != "" && button2.Text != "" && button3.Text != "" && button4.Text != "" && button5.Text != "" && button6.Text != "" && button7.Text != "" && button8.Text != "" && button9.Text != "")
+            {
+                MessageBox.Show("Empate");
+                reiniciarJuego();
+            }
+
+
+
+
+
+        }
+
+        //Funcion para cambiar el texto de los botones
+        public void cambiarTexto(Button boton)
+        {
+            byte turno = 1;
+
+            //Dos jugadores
+            if (boton.Text == "")
+            {
+                if (turno == 1)
+                {
+                    boton.Text = "X";
+                    turno = 2;
+                }
+                else
+                {
+                    boton.Text = "O";
+                    turno = 1;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Casilla ocupada");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cambiarTexto(button1);
+            comprobarGanador();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            cambiarTexto(button2);
+            comprobarGanador();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            cambiarTexto(button3);
+            comprobarGanador();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            cambiarTexto(button4);
+            comprobarGanador();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            cambiarTexto(button5);
+            comprobarGanador();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            cambiarTexto(button6);
+            comprobarGanador();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            cambiarTexto(button7);
+            comprobarGanador();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            cambiarTexto(button8);
+            comprobarGanador();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            cambiarTexto(button9);
+            comprobarGanador();
+        }
     }
 }
