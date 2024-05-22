6463c2a75e65e478733dd2a32e2e7ea6e26cef04
xoan.xc <61795755+xoanxc@users.noreply.github.com>
2024-05-22 23:33:56 +0200

    Sum_04

--- Tema 10/PROYECTO FINAL/Configurador.Designer.cs
+++ Tema 10/PROYECTO FINAL/Configurador.Designer.cs
@@ -36,7 +36,6 @@
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
-            this.groupBox5 = new System.Windows.Forms.GroupBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -46,11 +45,9 @@
             this.groupBox1.Controls.Add(this.rdIntel);
             this.groupBox1.Controls.Add(this.rdAMD);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(16, 15);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox1.Size = new System.Drawing.Size(267, 231);
+            this.groupBox1.Size = new System.Drawing.Size(200, 188);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Procesador";
@@ -58,20 +55,18 @@
             // cbProcesadores
             // 
             this.cbProcesadores.FormattingEnabled = true;
-            this.cbProcesadores.Location = new System.Drawing.Point(8, 101);
-            this.cbProcesadores.Margin = new System.Windows.Forms.Padding(4);
+            this.cbProcesadores.Location = new System.Drawing.Point(6, 82);
             this.cbProcesadores.Name = "cbProcesadores";
-            this.cbProcesadores.Size = new System.Drawing.Size(249, 33);
+            this.cbProcesadores.Size = new System.Drawing.Size(188, 28);
             this.cbProcesadores.TabIndex = 2;
             // 
             // rdIntel
             // 
             this.rdIntel.AutoSize = true;
             this.rdIntel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rdIntel.Location = new System.Drawing.Point(8, 69);
-            this.rdIntel.Margin = new System.Windows.Forms.Padding(4);
+            this.rdIntel.Location = new System.Drawing.Point(6, 56);
             this.rdIntel.Name = "rdIntel";
-            this.rdIntel.Size = new System.Drawing.Size(66, 24);
+            this.rdIntel.Size = new System.Drawing.Size(54, 20);
             this.rdIntel.TabIndex = 1;
             this.rdIntel.TabStop = true;
             this.rdIntel.Text = "Intel";
@@ -82,10 +77,9 @@
             // 
             this.rdAMD.AutoSize = true;
             this.rdAMD.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rdAMD.Location = new System.Drawing.Point(8, 41);
-            this.rdAMD.Margin = new System.Windows.Forms.Padding(4);
+            this.rdAMD.Location = new System.Drawing.Point(6, 33);
             this.rdAMD.Name = "rdAMD";
-            this.rdAMD.Size = new System.Drawing.Size(71, 24);
+            this.rdAMD.Size = new System.Drawing.Size(58, 20);
             this.rdAMD.TabIndex = 0;
             this.rdAMD.TabStop = true;
             this.rdAMD.Text = "AMD";
@@ -94,60 +88,41 @@
             // 
             // groupBox2
             // 
-            this.groupBox2.Location = new System.Drawing.Point(291, 15);
-            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Location = new System.Drawing.Point(218, 12);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox2.Size = new System.Drawing.Size(267, 123);
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Placa Base";
             // 
             // groupBox3
             // 
-            this.groupBox3.Location = new System.Drawing.Point(565, 15);
-            this.groupBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox3.Location = new System.Drawing.Point(424, 12);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox3.Size = new System.Drawing.Size(267, 123);
+            this.groupBox3.Size = new System.Drawing.Size(200, 100);
             this.groupBox3.TabIndex = 2;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Tarjeta Grafica";
             // 
             // groupBox4
             // 
-            this.groupBox4.Location = new System.Drawing.Point(16, 254);
-            this.groupBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox4.Location = new System.Drawing.Point(12, 206);
             this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox4.Size = new System.Drawing.Size(267, 123);
+            this.groupBox4.Size = new System.Drawing.Size(200, 100);
             this.groupBox4.TabIndex = 3;
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "Almacenamiento";
             // 
-            // groupBox5
-            // 
-            this.groupBox5.Location = new System.Drawing.Point(291, 254);
-            this.groupBox5.Margin = new System.Windows.Forms.Padding(4);
-            this.groupBox5.Name = "groupBox5";
-            this.groupBox5.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox5.Size = new System.Drawing.Size(267, 123);
-            this.groupBox5.TabIndex = 4;
-            this.groupBox5.TabStop = false;
-            this.groupBox5.Text = "Caja";
-            // 
             // Configurador
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
-            this.Controls.Add(this.groupBox5);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Configurador";
             this.Text = "Configurador";
             this.Load += new System.EventHandler(this.Configurador_Load);
@@ -166,6 +141,5 @@
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.GroupBox groupBox4;
-        private System.Windows.Forms.GroupBox groupBox5;
     }
 }
--- Tema 10/PROYECTO FINAL/MenuPrincipal.Designer.cs
+++ Tema 10/PROYECTO FINAL/MenuPrincipal.Designer.cs
@@ -36,7 +36,21 @@
             this.cerarSesiónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.configuradorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.gbProcesador = new System.Windows.Forms.GroupBox();
+            this.rdAMD = new System.Windows.Forms.RadioButton();
+            this.rdIntel = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.rdOficina = new System.Windows.Forms.RadioButton();
+            this.rdGamer = new System.Windows.Forms.RadioButton();
+            this.rdTrabajo = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.menuStrip1.SuspendLayout();
+            this.gbProcesador.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -47,7 +61,8 @@
             this.configuradorToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1067, 28);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -58,33 +73,33 @@
             this.acercaDeToolStripMenuItem,
             this.cerarSesiónToolStripMenuItem});
             this.opción1ToolStripMenuItem.Name = "opción1ToolStripMenuItem";
-            this.opción1ToolStripMenuItem.Size = new System.Drawing.Size(85, 24);
+            this.opción1ToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
             this.opción1ToolStripMenuItem.Text = "Opciones";
             // 
             // registrarUsuariosToolStripMenuItem
             // 
             this.registrarUsuariosToolStripMenuItem.Name = "registrarUsuariosToolStripMenuItem";
-            this.registrarUsuariosToolStripMenuItem.Size = new System.Drawing.Size(221, 26);
+            this.registrarUsuariosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.registrarUsuariosToolStripMenuItem.Text = "Gestión de usuarios";
             this.registrarUsuariosToolStripMenuItem.Click += new System.EventHandler(this.registrarUsuariosToolStripMenuItem_Click);
             // 
             // acercaDeToolStripMenuItem
             // 
             this.acercaDeToolStripMenuItem.Name = "acercaDeToolStripMenuItem";
-            this.acercaDeToolStripMenuItem.Size = new System.Drawing.Size(221, 26);
+            this.acercaDeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.acercaDeToolStripMenuItem.Text = "Acerca de...";
             // 
             // cerarSesiónToolStripMenuItem
             // 
             this.cerarSesiónToolStripMenuItem.Name = "cerarSesiónToolStripMenuItem";
-            this.cerarSesiónToolStripMenuItem.Size = new System.Drawing.Size(221, 26);
+            this.cerarSesiónToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.cerarSesiónToolStripMenuItem.Text = "Cerar sesión";
             this.cerarSesiónToolStripMenuItem.Click += new System.EventHandler(this.cerarSesiónToolStripMenuItem_Click);
             // 
             // configuradorToolStripMenuItem
             // 
             this.configuradorToolStripMenuItem.Name = "configuradorToolStripMenuItem";
-            this.configuradorToolStripMenuItem.Size = new System.Drawing.Size(111, 24);
+            this.configuradorToolStripMenuItem.Size = new System.Drawing.Size(90, 20);
             this.configuradorToolStripMenuItem.Text = "Configurador";
             this.configuradorToolStripMenuItem.Click += new System.EventHandler(this.configuradorToolStripMenuItem_Click);
             // 
@@ -94,19 +109,147 @@
             this.contextMenuStrip1.Name = "contextMenuStrip1";
             this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(243, 57);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(227, 277);
+            this.listBox1.TabIndex = 2;
+            // 
+            // gbProcesador
+            // 
+            this.gbProcesador.Controls.Add(this.rdIntel);
+            this.gbProcesador.Controls.Add(this.rdAMD);
+            this.gbProcesador.Location = new System.Drawing.Point(27, 57);
+            this.gbProcesador.Name = "gbProcesador";
+            this.gbProcesador.Size = new System.Drawing.Size(200, 130);
+            this.gbProcesador.TabIndex = 3;
+            this.gbProcesador.TabStop = false;
+            this.gbProcesador.Text = "Procesador";
+            // 
+            // rdAMD
+            // 
+            this.rdAMD.AutoSize = true;
+            this.rdAMD.Location = new System.Drawing.Point(6, 58);
+            this.rdAMD.Name = "rdAMD";
+            this.rdAMD.Size = new System.Drawing.Size(49, 17);
+            this.rdAMD.TabIndex = 0;
+            this.rdAMD.TabStop = true;
+            this.rdAMD.Text = "AMD";
+            this.rdAMD.UseVisualStyleBackColor = true;
+            // 
+            // rdIntel
+            // 
+            this.rdIntel.AutoSize = true;
+            this.rdIntel.Location = new System.Drawing.Point(6, 81);
+            this.rdIntel.Name = "rdIntel";
+            this.rdIntel.Size = new System.Drawing.Size(45, 17);
+            this.rdIntel.TabIndex = 1;
+            this.rdIntel.TabStop = true;
+            this.rdIntel.Text = "Intel";
+            this.rdIntel.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 33);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(85, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(6, 56);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(85, 17);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "radioButton4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // rdOficina
+            // 
+            this.rdOficina.AutoSize = true;
+            this.rdOficina.Location = new System.Drawing.Point(31, 81);
+            this.rdOficina.Name = "rdOficina";
+            this.rdOficina.Size = new System.Drawing.Size(49, 17);
+            this.rdOficina.TabIndex = 4;
+            this.rdOficina.TabStop = true;
+            this.rdOficina.Text = "500€";
+            this.rdOficina.UseVisualStyleBackColor = true;
+            // 
+            // rdGamer
+            // 
+            this.rdGamer.AutoSize = true;
+            this.rdGamer.Location = new System.Drawing.Point(31, 113);
+            this.rdGamer.Name = "rdGamer";
+            this.rdGamer.Size = new System.Drawing.Size(55, 17);
+            this.rdGamer.TabIndex = 5;
+            this.rdGamer.TabStop = true;
+            this.rdGamer.Text = "1000€";
+            this.rdGamer.UseVisualStyleBackColor = true;
+            // 
+            // rdTrabajo
+            // 
+            this.rdTrabajo.AutoSize = true;
+            this.rdTrabajo.Location = new System.Drawing.Point(31, 136);
+            this.rdTrabajo.Name = "rdTrabajo";
+            this.rdTrabajo.Size = new System.Drawing.Size(55, 17);
+            this.rdTrabajo.TabIndex = 6;
+            this.rdTrabajo.TabStop = true;
+            this.rdTrabajo.Text = "1500€";
+            this.rdTrabajo.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdOficina);
+            this.groupBox1.Controls.Add(this.rdTrabajo);
+            this.groupBox1.Controls.Add(this.rdGamer);
+            this.groupBox1.Location = new System.Drawing.Point(486, 57);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(218, 278);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton3);
+            this.groupBox2.Controls.Add(this.radioButton4);
+            this.groupBox2.Location = new System.Drawing.Point(27, 193);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
             // MenuPrincipal
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.gbProcesador);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "MenuPrincipal";
             this.Text = "MenuPrincipal";
             this.Load += new System.EventHandler(this.MenuPrincipal_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.gbProcesador.ResumeLayout(false);
+            this.gbProcesador.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -121,5 +264,16 @@
         private System.Windows.Forms.ToolStripMenuItem acercaDeToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem cerarSesiónToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem configuradorToolStripMenuItem;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox gbProcesador;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton rdIntel;
+        private System.Windows.Forms.RadioButton rdAMD;
+        private System.Windows.Forms.RadioButton rdOficina;
+        private System.Windows.Forms.RadioButton rdGamer;
+        private System.Windows.Forms.RadioButton rdTrabajo;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
